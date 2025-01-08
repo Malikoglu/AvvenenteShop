5faed02ec3479bcaec7870138f66e4eb745fd25e
Ömer Faruk Malikoğlu <malikogluomerfaruk@gmail.com>
2025-01-08 11:41:09 +0300

    Product Classes Created

---
+++ Services/Catalog/AvvenenteShop.Catalog/Entities/ProductDetail.cs
@@ -0,0 +1,11 @@
+﻿namespace AvvenenteShop.Catalog.Entities
+{
+    public class ProductDetail
+    {
+        public string ProductDetailID { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProducrInfo { get; set; }
+
+
+    }
+}
---
+++ Services/Catalog/AvvenenteShop.Catalog/Entities/ProductImages.cs
@@ -0,0 +1,14 @@
+﻿namespace AvvenenteShop.Catalog.Entities
+{
+    public class ProductImages
+    {
+        public string ProductImageID { get; set; }
+        public string Image1 { get; set; }
+        public string Image2 { get; set; }
+        public string Image3 { get; set; }
+        public string ProductId { get; set; }
+        public Product Product { get; set; }
+
+
+    }
+}
